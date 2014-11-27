7b061810a7e3f25c545154f4dd1e90eae31f1d21
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2014-11-27 18:45:50 -0300

    Initial commit

---
+++ app/recipes/deploy.rb
@@ -0,0 +1,11 @@
+# app/recipes/deploy.rb
+#
+# Cookbook Name:: app
+# Recipe:: deploy
+#
+
+# This deploys the application
+opsworks_play2 do
+  app "app"
+  deploy_data node[:deploy][:app] # This data comes from OpsWorks' JSON blob
+end
---
+++ app/recipes/setup.rb
@@ -0,0 +1,8 @@
+# app/recipes/setup.rb
+#
+# Cookbook Name:: app
+# Recipe:: setup
+#
+
+# This installs the play framework
+include_recipe "play2::setup"
