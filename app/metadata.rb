499984eb12cf19bd972c83a97eb6d0bc804e9a9d
Gonzalo Bellver <gonzalo.bellver@gmail.com>
2014-12-01 12:57:28 -0300

    Add play2 dependency

--- app/metadata.rb
+++ app/metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures app'
 long_description 'Installs/Configures app'
 version          '0.1.0'
+
+depends "play2", "~> 0.1.0" # add a dependency on the play2 cookbook
